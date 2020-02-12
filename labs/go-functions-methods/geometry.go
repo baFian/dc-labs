feeeadae267b7bebfbd996fb4da24ece2bc2c51e
Fabian Adolfo Cervantes Romero <0209134@up.edu.mx>
2020-02-12 10:52:49 -0600

    Tarea

--- labs/go-functions-methods/geometry.go
+++ labs/go-functions-methods/geometry.go
@@ -1,24 +1,33 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 156.
-
 // Package geometry defines simple types for plane geometry.
 //!+point
-package geometry
+package main
 
-import "math"
-
-type Point struct{ X, Y float64 }
+import (
+	"math"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+    
+)
+type Point struct{ x, y float64 }
 
 // traditional function
 func Distance(p, q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	return math.Hypot(q.x-p.x, q.y-p.y)
 }
 
 // same thing, but as a method of the Point type
 func (p Point) Distance(q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	return math.Hypot(q.x-p.x, q.y-p.y)
+}
+
+func (p Point) X()float64{
+	return p.x;
+}
+func (p Point) Y()float64{
+	return p.y;
 }
 
 //!-point
@@ -38,5 +47,29 @@ func (path Path) Distance() float64 {
 	}
 	return sum
 }
+
+
+func main(){
+	
+	
+	sides, _ := strconv.Atoi(os.Args[1])
+	Vertex :=  Path{}
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	for i:=0;i<sides;i++{
+		
+		x:=r1.Float64()*5
+		y:=r1.Float64()*5
+		point :=Point{x, y}
+		Vertex = append(Vertex, point)
+		
+	}
+	fmt.Print(Vertex)
+		
+	fmt.Print(Vertex.Distance())
+	
+}
+
+
 
 //!-path
